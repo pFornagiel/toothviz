1a30de71d81f7632760cb59cc9c81833a492e3df
Massny <pawel.fornagiel2@gmail.com>
2026-06-06 13:26:43 +0200

    feat: add tooth icon to page-laout

--- packages/application/frontend/src/app/components/layout/page-layout.tsx
+++ packages/application/frontend/src/app/components/layout/page-layout.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
 import { Button } from "../ui/button";
+import { Tooth } from "../icons/tooth";
 interface PageLayoutProps {
   title?: string;
   showBackButton?: boolean;
@@ -32,19 +33,21 @@ export function PageLayout({
     <div className="min-h-screen bg-background flex flex-col font-sans">
       <header className="border-b border-border bg-card px-6 py-4 shadow-sm">
         <div className="flex items-center gap-4">
-          {showBackButton && (
-            // <Button
-            // variant="ghost"
-            // className="absolute top-2 right-2 h-8 w-8 p-0"
-            // >
-            <Button
-              variant="ghost"
-              onClick={handleBack}
-              className="h-8 w-8 cursor-pointer text-foreground p-1"
-            >
+          <Button
+            variant="ghost"
+            onClick={handleBack}
+            className={cn(
+              "h-8 w-8 cursor-pointer text-foreground p-1",
+              !showBackButton ? "pointer-events-none" : "",
+            )}
+          >
+            {showBackButton ? (
               <ChevronLeft className="w-8 h-7 text-foreground cursor-pointer" />
-            </Button>
-          )}
+            ) : (
+              <Tooth className="!w-7 !h-7 text-foreground cursor-pointer" />
+            )}
+          </Button>
+
           <h1 className="text-xl text-foreground font-semibold tracking-tight">{title}</h1>
         </div>
       </header>
